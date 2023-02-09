1c4d7645c5f178d777754dbff2c3503daad4d98c
xicilion <lion@9465.net>
2023-02-09 18:33:40 +0000

    build, refactor: refactor glibc detect function.

--- cmake/option_config.cmake
+++ cmake/option_config.cmake
@@ -1,12 +1,16 @@
 
-function(check_glibc func ver flag)
-    unset(HAVE_GLIB_C_${func} CACHE)
-    check_c_source_compiles("void ${func}();
-        __asm__(\".symver ${func},${func}@GLIBC_${ver}\");
-        int main(void){${func}();return 0;}" HAVE_GLIB_C_${func})
-    if(${HAVE_GLIB_C_${func}})
-        set(${flag} "${ver}" PARENT_SCOPE)
-    endif()
+function(check_glibc func flag)
+    set(vers 2.27 2.17 2.4 2.2.5 2.2 2.0)
+
+    foreach(ver ${vers})
+        unset(HAVE_GLIB_C_${func} CACHE)
+        check_c_source_compiles("void ${func}();
+            __asm__(\".symver ${func},${func}@GLIBC_${ver}\");
+            int main(void){${func}();return 0;}" HAVE_GLIB_C_${func})
+        if(${HAVE_GLIB_C_${func}})
+            set(${flag} "${ver}" PARENT_SCOPE)
+        endif()
+    endforeach()
 endfunction()
 
 # run c code to get some library information(like iconv/glibc) from env
@@ -28,21 +32,10 @@ function(config)
     endif()
 
     if("${BUILD_OS}" STREQUAL "Linux")
-        check_glibc(memcpy 2.2.5 GLIB_C_MEMCPY)
-
-        check_glibc(fcntl 2.17 GLIB_C_FCNTL)
-        check_glibc(fcntl 2.4 GLIB_C_FCNTL)
-        check_glibc(fcntl 2.2.5 GLIB_C_FCNTL)
-        check_glibc(fcntl 2.0 GLIB_C_FCNTL)
-
-        check_glibc(pow 2.17 GLIB_C_MATH)
-        check_glibc(pow 2.4 GLIB_C_MATH)
-        check_glibc(pow 2.2.5 GLIB_C_MATH)
-        check_glibc(pow 2.0 GLIB_C_MATH)
-
-        check_glibc(clock_gettime 2.4 GLIB_C_TIME)
-        check_glibc(clock_gettime 2.2.5 GLIB_C_TIME)
-        check_glibc(clock_gettime 2.2 GLIB_C_TIME)
+        check_glibc(memcpy GLIB_C_MEMCPY)
+        check_glibc(fcntl GLIB_C_FCNTL)
+        check_glibc(pow GLIB_C_MATH)
+        check_glibc(clock_gettime GLIB_C_TIME)
     endif()
 
     if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/tools/config.h.in)
